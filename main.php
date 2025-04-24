bc5af1140a64876f04e40409bfc7888a52184297
pauca881 <lopezpaud@gmail.com>
2025-04-24 21:36:55 +0200

    logout

--- main.php
+++ main.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
 
+
 if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['cerrar_sesion'])) {
+    session_destroy();
     header("Location: login.php");
     exit();
 }
@@ -14,4 +21,9 @@ if (!isset($_SESSION['user'])) {
 
 <form action="crear_post.php" method="get">
     <input type="submit" value="Crear nuevo post">
+</form>
+
+<!-- Botón para cerrar sesión -->
+<form method="post">
+    <input type="submit" name="cerrar_sesion" value="Cerrar sesión">
 </form>
