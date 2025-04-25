14aa1f04c5a6c0ffb3a330a978b46b11830db9be
pauca881 <lopezpaud@gmail.com>
2025-04-25 18:33:58 +0200

    crear post

--- crear_post.php
+++ crear_post.php
@@ -10,3 +10,16 @@ if (!isset($_SESSION['user'])) {
 
 
 ?>
+
+<form action="crear_post.php" method="post">
+    <label>Títol:</label><br>
+    <input type="text" name="titulo" required><br><br>
+    
+    <label>Contenido:</label><br>
+    <textarea name="contenido" required></textarea><br><br>
+
+    <label>Post creado por:</label><br>
+    <input type="text" name="user" value="<?php echo $_SESSION['user']; ?>" readonly><br><br>
+
+    <input type="submit" value="Publicar">
+</form>
